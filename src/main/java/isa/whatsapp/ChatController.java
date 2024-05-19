857a94dbf5024e6862e900fdf759e0efb40b989d
Berubu <93852000+Berubu@users.noreply.github.com>
2024-05-18 23:16:37 -0600

    Update ChatController.java

--- src/main/java/isa/whatsapp/ChatController.java
+++ src/main/java/isa/whatsapp/ChatController.java
@@ -1,5 +1,4 @@
 package isa.whatsapp;
-
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,7 +10,6 @@ import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -38,30 +36,26 @@ public class ChatController {
     private int colorIndex = 0;
     private String[] colors =
             {"#E6E6FA", "#F0E68C", "#FFB6C1", "#87CEEB", "#FFA07A", "#98FB98", "#DDA0DD", "#FA8072", "#FFD700"};
-
     public void initializeChat(String name, String ip, int port) {
         this.userName = name;
 
         Image image = new Image("file:src/main/resources/isa/whatsapp/3.jpg");
         backgroundImage.setImage(image);
-
         try {
             Socket socket = new Socket(ip, port);
             out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out.println(name);
 
-            // Hilo para escuchar los mensajes dl servidor
+            // Hilo para rrecivir los mensajes dl servidor
             new Thread(() -> {
                 String msg;
                 try {
                     while ((msg = in.readLine()) != null) {
                         String finalMsg = msg;
-                        Platform.runLater(() -> addMessage(finalMsg, !finalMsg.startsWith(userName + ":")));
-                    }
+                        Platform.runLater(() -> addMessage(finalMsg, !finalMsg.startsWith(userName + ":")));}
                 } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                    e.printStackTrace();}
             }).start();
 
             sendButton.setOnAction(event -> {
@@ -72,12 +66,10 @@ public class ChatController {
                     messageField.clear();
                 }
             });
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
     private void addMessage(String message, boolean isReceived) {
         HBox hBox = new HBox();
         Label label = new Label(message);
@@ -91,7 +83,6 @@ public class ChatController {
         }
 
         label.setStyle("-fx-background-color: " + userColors.get(user) + "; -fx-padding: 5px; -fx-background-radius: 10px;");
-
         if (isReceived) {
             hBox.setStyle("-fx-alignment: center-left;");
         } else {
